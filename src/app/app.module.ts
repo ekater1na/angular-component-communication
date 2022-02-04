20464726bb0e5d1d6f063eb92ddd6b5fc4f432a2
ekater1na <ekaterina_l@tut.by>
2022-02-04 17:42:26 +0300

    refactor: remove module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-// Imports for loading & configuring the in-memory web api
-import { HttpClientInMemoryWebApiModule  } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -21,7 +17,6 @@ import { UserModule } from './user/user.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
     AppRoutingModule
   ],
